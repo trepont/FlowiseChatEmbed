1851e6bfc1944cd2606c33c82c5494d5944ab038
chanaiah.k <chanaiah.k@mfino.mobi>
2023-06-22 17:15:04 +0530

    added custom text

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -61,12 +61,7 @@ export const Badge = (props: Props) => {
         id="lite-badge"
         style={{ "font-weight": 'bold', color: props.poweredByTextColor ?? defaultTextColor }}
       >
-        <span style="padding-right:3px; padding-top: 3px; display:inline-block;">
-
-<img class="manImg" src="http://35.154.25.222:8090/treepont_logo.jpeg"></img>
-
-</span>
-
+        <span> trepont.ai </span>
       </a>
     </span>
   )
